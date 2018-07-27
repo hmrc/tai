d8a37584e9395cebe1b5685d5193fa80e0479586
Amaal Ali <amaal.ali@opencastsoftware.com>
2018-07-27 16:21:55 +0100

    ST-136: Refactor connector to use http handler and parse response.

--- app/uk/gov/hmrc/tai/connectors/TaxCodeChangeConnector.scala
+++ app/uk/gov/hmrc/tai/connectors/TaxCodeChangeConnector.scala
@@ -17,32 +17,49 @@
 package uk.gov.hmrc.tai.connectors
 
 import com.google.inject.{Inject, Singleton}
-import play.api.libs.json.{JsValue, Json}
-import uk.gov.hmrc.domain.{Generator, Nino}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.tai.config.NpsConfig
+import uk.gov.hmrc.tai.model.enums.APITypes
 import uk.gov.hmrc.tai.model.tai.TaxYear
 
 import scala.concurrent.Future
-import scala.util.Random
+
+case class TaxCodeHistoryItem(employmentId: Int, p2Issued: Boolean, p2Date: String)
+
+object TaxCodeHistoryItem {
+  implicit val reads: Reads[TaxCodeHistoryItem] = (
+    (__ \ "employmentId").read[Int] and
+      (__ \ "p2Issued").read[Boolean] and
+      (__ \ "p2Date").read[String]
+    )(TaxCodeHistoryItem.apply _)
+}
+
+case class TaxCodeHistoryDetails(nino: Nino, taxCodeHistoryItems: Seq[TaxCodeHistoryItem])
+
+object TaxCodeHistoryDetails {
+
+  implicit val reads: Reads[TaxCodeHistoryDetails] = (
+    (__ \ "nino").read[Nino] and
+      (__ \ "taxHistoryList").read[Seq[TaxCodeHistoryItem]]
+    )(TaxCodeHistoryDetails.apply _)
+}
 
 @Singleton
 class TaxCodeChangeConnector @Inject()(config: NpsConfig,
-                                       taxCodeChangeUrl: TaxCodeChangeUrl,
+                                       urlConfig: TaxCodeChangeUrl,
                                        httpHandler: HttpHandler){
 
-  def taxCodeChanges(nino: Nino, taxYear: TaxYear)(implicit hc: HeaderCarrier): Future[JsValue] = {
+  def taxCodeChanges(nino: Nino, taxYear: TaxYear)(implicit hc: HeaderCarrier): Future[TaxCodeHistoryDetails] = {
 
-    val response = Json.obj(
-      "nino" -> new Generator(new Random).nextNino,
-      "taxHistoryList" -> Seq(
-        Json.obj(
-          "employmentId" -> 1234567890,
-          "p2Issued" -> true
-        )
-      )
-    )
+    val url = s"http://localhost:9332/personal-tax-account/tax-code/history/api/v1/${nino.nino}/${taxYear.year}"
 
-    Future.successful(response)
+    httpHandler.getFromApi(url, APITypes.TaxCodeChangeAPI) map {
+      x => x.as[TaxCodeHistoryDetails]
+    }
+
   }
 }
