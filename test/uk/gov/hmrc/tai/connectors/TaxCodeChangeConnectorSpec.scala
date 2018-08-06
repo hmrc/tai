79324d988853d17593726dec3363870fd0da0dd3
Amaal Ali <amaal.ali@opencastsoftware.com>
2018-08-06 16:07:01 +0100

    ST-136: Add for TaxCodeChangeConnectorSpec TaxCodeChangeControllerSpec and TaxCodeChangeServiceImplSpec.

---
+++ test/uk/gov/hmrc/tai/connectors/TaxCodeChangeConnectorSpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tai.connectors
+
+import java.net.URL
+
+import org.joda.time.LocalDate
+import com.github.tomakehurst.wiremock.client.WireMock.{get, ok, urlEqualTo}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+import uk.gov.hmrc.domain.{Generator, Nino}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.tai.audit.Auditor
+import uk.gov.hmrc.tai.config.NpsJsonServiceConfig
+import uk.gov.hmrc.tai.metrics.Metrics
+import uk.gov.hmrc.tai.model.{TaxCodeHistory, TaxCodeRecord}
+import uk.gov.hmrc.tai.model.tai.TaxYear
+import uk.gov.hmrc.tai.util.WireMockHelper
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.Random
+
+class TaxCodeChangeConnectorSpec extends PlaySpec with WireMockHelper with BeforeAndAfterAll with MockitoSugar {
+
+
+  def config = injector.instanceOf[TaxCodeChangeUrl]
+  val testNino = randomNino
+
+  "tax code change API" must {
+    "return tax code change response" in {
+
+      val metrics =  injector.instanceOf[Metrics]
+      val httpClient =  injector.instanceOf[HttpClient]
+      val auditor =  injector.instanceOf[Auditor]
+      val npsConfig =  injector.instanceOf[NpsJsonServiceConfig]
+      val taxYear = TaxYear(2017)
+
+      val url = new URL(config.taxCodeChangeUrl(testNino, taxYear))
+
+      server.stubFor(
+        get(urlEqualTo(url.getPath())).willReturn(ok(jsonResponse.toString))
+      )
+
+      val result = Await.result(createSut(
+        metrics,
+        httpClient,
+        auditor,
+        npsConfig,
+        config).taxCodeHistory(testNino, taxYear), 10.seconds)
+
+      result mustEqual TaxCodeHistory(testNino.nino, Some(Seq(TaxCodeRecord("1185L", "Employer 1", "operated", LocalDate.parse("2017-06-23")))))
+    }
+
+  }
+
+
+
+  private val jsonResponse = Json.obj(
+    "nino" -> testNino.nino,
+    "taxCodeRecord" -> Seq(
+      Json.obj(
+        "taxCode" -> "1185L",
+        "employerName" -> "Employer 1",
+        "operatedTaxCode" -> "operated",
+        "p2Issued" -> true,
+        "p2Date" -> "2017-06-23"
+      )
+    )
+  )
+
+  implicit val headerCarrier: HeaderCarrier = mock[HeaderCarrier]
+
+  private def randomNino: Nino = new Generator(new Random).nextNino
+
+  private def createSut(
+                         metrics: Metrics,
+                         httpClient: HttpClient,
+                         auditor: Auditor,
+                         config: NpsJsonServiceConfig,
+                         taxCodeChangeUrl: TaxCodeChangeUrl) =
+    new TaxCodeChangeConnector(metrics, httpClient, auditor, config, taxCodeChangeUrl)
+
+
+}
---
+++ test/uk/gov/hmrc/tai/controllers/taxCodeChange/TaxCodeChangeControllerSpec.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tai.controllers.taxCodeChange
+
+import org.joda.time.LocalDate
+import org.mockito.Mockito.when
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{status, _}
+import uk.gov.hmrc.domain.Generator
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.tai.config.FeatureTogglesConfig
+import uk.gov.hmrc.tai.mocks.MockAuthenticationPredicate
+import uk.gov.hmrc.tai.model.api.ApiResponse
+import uk.gov.hmrc.tai.model.{TaxCodeHistory, TaxCodeRecord}
+import uk.gov.hmrc.tai.service.TaxCodeChangeService
+
+import scala.concurrent.Future
+import scala.util.Random
+
+class TaxCodeChangeControllerSpec extends PlaySpec with MockitoSugar with MockAuthenticationPredicate {
+
+  "hasTaxCodeChanged" should {
+
+    "return true" when {
+      "there has been a tax code change" in {
+
+        val testNino = ninoGenerator
+
+        when(mockConfig.taxCodeChangeEnabled).thenReturn(true)
+        when(mockTaxCodeService.hasTaxCodeChanged(testNino)).thenReturn(Future.successful(true))
+
+        val response: Future[Result] = controller.hasTaxCodeChanged(testNino)(FakeRequest())
+
+        status(response) mustBe OK
+
+        contentAsJson(response) mustEqual Json.toJson(true)
+      }
+    }
+
+    "return false" when {
+      "there has not been a tax code change" in {
+
+        val testNino = ninoGenerator
+
+        when(mockConfig.taxCodeChangeEnabled).thenReturn(true)
+        when(mockTaxCodeService.hasTaxCodeChanged(testNino)).thenReturn(Future.successful(false))
+
+        val response: Future[Result] = controller.hasTaxCodeChanged(testNino)(FakeRequest())
+
+        status(response) mustBe OK
+
+        contentAsJson(response) mustEqual Json.toJson(false)
+      }
+    }
+
+    "return false" when {
+      "taxCodeChanged endpoint is toggled off" in {
+
+        val testNino = ninoGenerator
+
+        when(mockConfig.taxCodeChangeEnabled).thenReturn(false)
+
+        val response: Future[Result] = controller.hasTaxCodeChanged(testNino)(FakeRequest())
+
+        status(response) mustBe OK
+
+        contentAsJson(response) mustEqual Json.toJson(false)
+      }
+    }
+  }
+
+  "taxCodeHistory" should {
+    "return given nino's tax code history" in {
+
+      val testNino = ninoGenerator
+
+      val taxCodeHistory =
+        TaxCodeHistory(
+          testNino.nino,
+          Some(Seq(
+            TaxCodeRecord(taxCode="1185L", employerName="employer2", operatedTaxCode="operated", p2Date=new LocalDate(2018, 7, 11)),
+            TaxCodeRecord(taxCode="1080L", employerName="employer1", operatedTaxCode="operated", p2Date=new LocalDate(2018, 4, 11))
+          ))
+        )
+
+      when(mockTaxCodeService.taxCodeHistory(testNino)).thenReturn(Future.successful(taxCodeHistory))
+
+      val response = controller.taxCodeHistory(testNino)(FakeRequest())
+
+      contentAsJson(response) mustEqual Json.toJson(ApiResponse(taxCodeHistory, Nil))
+
+    }
+  }
+
+  implicit val hc = HeaderCarrier()
+  val mockConfig = mock[FeatureTogglesConfig]
+  val mockTaxCodeService = mock[TaxCodeChangeService]
+
+  private def controller = new TaxCodeChangeController(loggedInAuthenticationPredicate, mockTaxCodeService, mockConfig)
+  private def ninoGenerator = new Generator(new Random).nextNino
+}
