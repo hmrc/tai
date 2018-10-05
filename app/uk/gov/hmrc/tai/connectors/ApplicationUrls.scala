db3e2b19479495aaa6cf058dc066a1f147b13ac6
Oktawian Cirko <oktawiancirko@Administrators-MacBook-Pro-2.local>
2018-10-05 10:23:26 +0100

    changes URL from hypothethical to real

--- app/uk/gov/hmrc/tai/connectors/ApplicationUrls.scala
+++ app/uk/gov/hmrc/tai/connectors/ApplicationUrls.scala
@@ -75,10 +75,10 @@ class BbsiUrls @Inject()(config: DesConfig) {
 class TaxAccountUrls @Inject()(npsConfig: NpsConfig, desConfig: DesConfig) {
 
   def taxAccountUrlNps(nino: Nino, taxYear: TaxYear): String =
-    s"${npsConfig.baseURL}/person/${nino.nino}/tax-account/${taxYear.year}/calculation"
+    s"${npsConfig.baseURL}/person/${nino.nino}/tax-account/${taxYear.year}"
 
   def taxAccountUrlDes(nino: Nino, taxYear: TaxYear): String =
-    s"${desConfig.baseURL}/pay-as-you-earn/individuals/${nino.nino}/tax-account/tax-year/${taxYear.year}?calculation=true"
+    s"${desConfig.baseURL}/pay-as-you-earn/individuals/${nino.nino}/tax-account/tax-year/${taxYear.year}"
 }
 
 @Singleton
