42498edbe961c98361cda0db82fc8299e1b7a2dc
John Lewis <23009934+lewey@users.noreply.github.com>
2019-02-08 10:52:27 +0000

    ST-534:Removal of cache invalidation after performing a estimated pay update

--- app/uk/gov/hmrc/tai/controllers/income/IncomeController.scala
+++ app/uk/gov/hmrc/tai/controllers/income/IncomeController.scala
@@ -68,9 +68,7 @@ class IncomeController @Inject()(incomeService: IncomeService,
       implicit request =>
         withJsonBody[UpdateTaxCodeIncomeRequest] { updateTaxCodeIncomeRequest =>
           incomeService.updateTaxCodeIncome(nino, snapshotId, employmentId, updateTaxCodeIncomeRequest.amount) map {
-            case IncomeUpdateSuccess =>
-              taxAccountService.invalidateTaiCacheData()
-              Ok
+            case IncomeUpdateSuccess => Ok
             case InvalidAmount(message) => BadRequest(message)
             case IncomeUpdateFailed(message) => InternalServerError(message)
           }
