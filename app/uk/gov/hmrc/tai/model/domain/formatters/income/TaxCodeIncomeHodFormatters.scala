cfbc3d5333c1e87ba08f0656f12804515f125ef8
John Lewis <23009934+lewey@users.noreply.github.com>
2019-03-19 11:52:32 +0000

    employment-status-logging:Further logging to give more context

--- app/uk/gov/hmrc/tai/model/domain/formatters/income/TaxCodeIncomeHodFormatters.scala
+++ app/uk/gov/hmrc/tai/model/domain/formatters/income/TaxCodeIncomeHodFormatters.scala
@@ -87,6 +87,7 @@ trait TaxCodeIncomeHodFormatters {
       case Some(3) => Ceased
       case default => {
         Logger.warn(s"Invalid Employment Status -> $default")
+        Logger.warn(s"Json -> $json")
         throw new RuntimeException("Invalid employment status")
       }
     }
