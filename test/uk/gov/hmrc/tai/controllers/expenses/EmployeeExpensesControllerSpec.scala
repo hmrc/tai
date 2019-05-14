731e31b8052344cfefbe153dfe3479ddda38f798
Thomas Pascoe <15924950+Pascoe121@users.noreply.github.com>
2019-05-14 15:06:48 +0100

    EE-524 Added tests for EEController and EEService

---
+++ test/uk/gov/hmrc/tai/controllers/expenses/EmployeeExpensesControllerSpec.scala
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tai.controllers.expenses
+
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.ScalaFutures._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsNull, Json}
+import play.api.test.Helpers._
+import play.api.test.{FakeHeaders, FakeRequest}
+import uk.gov.hmrc.auth.core.MissingBearerToken
+import uk.gov.hmrc.domain.Generator
+import uk.gov.hmrc.http.logging.SessionId
+import uk.gov.hmrc.http.{BadRequestException, HeaderCarrier, HttpResponse, NotFoundException}
+import uk.gov.hmrc.tai.controllers.predicates.AuthenticationPredicate
+import uk.gov.hmrc.tai.mocks.MockAuthenticationPredicate
+import uk.gov.hmrc.tai.model.IabdUpdateExpensesRequest
+import uk.gov.hmrc.tai.model.nps.NpsIabdRoot
+import uk.gov.hmrc.tai.model.tai.TaxYear
+import uk.gov.hmrc.tai.service.expenses.EmployeeExpensesService
+
+import scala.concurrent.Future
+import scala.util.Random
+
+class EmployeeExpensesControllerSpec extends PlaySpec
+  with MockitoSugar
+  with MockAuthenticationPredicate {
+
+  private implicit val hc: HeaderCarrier = HeaderCarrier(sessionId = Some(SessionId("TEST")))
+
+  private val mockEmployeeExpensesService = mock[EmployeeExpensesService]
+
+  private def controller(authentication: AuthenticationPredicate = loggedInAuthenticationPredicate) =
+    new EmployeeExpensesController(authentication, employeeExpensesService = mockEmployeeExpensesService)
+
+  private val nino = new Generator(new Random).nextNino
+  private val iabd = 56
+  private val iabdUpdateExpensesRequest = IabdUpdateExpensesRequest(1, grossAmount = 100)
+
+  private val taxYear = 2017
+
+  private val validNpsIabd = List(
+    NpsIabdRoot(
+      nino = nino.withoutSuffix,
+      `type` = 56,
+      grossAmount = Some(100)
+    )
+  )
+
+  private val validJson = Json.toJson(validNpsIabd)
+
+  "updateEmployeeExpensesData" must {
+
+    "return NO CONTENT" when {
+      "a valid update amount is provided and a OK response is returned" in {
+        val fakeRequest = FakeRequest("POST", "/", FakeHeaders(), Json.toJson(iabdUpdateExpensesRequest))
+          .withHeaders(("content-type", "application/json"))
+
+        when(mockEmployeeExpensesService.updateEmployeeExpensesData(any(), any(), any(), any(), any())(any()))
+          .thenReturn(Future.successful(HttpResponse(200)))
+
+        val result = controller().updateEmployeeExpensesData(nino, TaxYear(), iabd)(fakeRequest)
+
+        status(result) mustBe NO_CONTENT
+      }
+
+      "a valid update amount is provided and a NO CONTENT response is returned" in {
+        val fakeRequest = FakeRequest("POST", "/", FakeHeaders(), Json.toJson(iabdUpdateExpensesRequest))
+          .withHeaders(("content-type", "application/json"))
+
+        when(mockEmployeeExpensesService.updateEmployeeExpensesData(any(), any(), any(), any(), any())(any()))
+          .thenReturn(Future.successful(HttpResponse(204)))
+
+        val result = controller().updateEmployeeExpensesData(nino, TaxYear(), iabd)(fakeRequest)
+
+        status(result) mustBe NO_CONTENT
+      }
+
+      "a valid update amount is provided and a ACCEPTED response is returned" in {
+        val fakeRequest = FakeRequest("POST", "/", FakeHeaders(), Json.toJson(iabdUpdateExpensesRequest))
+          .withHeaders(("content-type", "application/json"))
+
+        when(mockEmployeeExpensesService.updateEmployeeExpensesData(any(), any(), any(), any(), any())(any()))
+          .thenReturn(Future.successful(HttpResponse(202)))
+
+        val result = controller().updateEmployeeExpensesData(nino, TaxYear(), iabd)(fakeRequest)
+
+        status(result) mustBe NO_CONTENT
+      }
+    }
+
+    "return BAD REQUEST" when {
+      "invalid update amount is provided" in {
+        val fakeRequest = FakeRequest("POST", "/", FakeHeaders(), Json.toJson(""))
+          .withHeaders(("content-type", "application/json"))
+
+        when(mockEmployeeExpensesService.updateEmployeeExpensesData(any(), any(), any(), any(), any())(any()))
+          .thenReturn(Future.successful(HttpResponse(200)))
+
+        val result = controller().updateEmployeeExpensesData(nino, TaxYear(), iabd)(fakeRequest)
+
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return NOT AUTHORISED" when {
+      "the user is not logged in" in {
+        val fakeRequest = FakeRequest("POST", "/", FakeHeaders(), JsNull)
+          .withHeaders(("content-type", "application/json"))
+
+        when(mockEmployeeExpensesService.updateEmployeeExpensesData(any(), any(), any(), any(), any())(any()))
+          .thenReturn(Future.successful(HttpResponse(200)))
+
+        val result = controller(notLoggedInAuthenticationPredicate).updateEmployeeExpensesData(nino, TaxYear(), iabd)(fakeRequest)
+
+        whenReady(result.failed) {
+          e => e mustBe a[MissingBearerToken]
+        }
+      }
+    }
+
+    "return INTERNAL SERVER ERROR" when {
+      "flat rate expenses update exception has been thrown" in {
+        val fakeRequest = FakeRequest("POST", "/", FakeHeaders(), Json.toJson(iabdUpdateExpensesRequest))
+          .withHeaders(("content-type", "application/json"))
+
+        when(mockEmployeeExpensesService.updateEmployeeExpensesData(any(), any(), any(), any(), any())(any()))
+          .thenReturn(Future.successful(HttpResponse(500)))
+
+        val result = controller().updateEmployeeExpensesData(nino, TaxYear(), iabd)(fakeRequest)
+
+        status(result) mustBe INTERNAL_SERVER_ERROR
+      }
+    }
+
+  }
+
+  "getEmployeeExpensesData" must {
+
+    "return OK and valid json" in {
+      val fakeRequest = FakeRequest("GET", "/", FakeHeaders(), any())
+
+      when(mockEmployeeExpensesService.getEmployeeExpenses(any(), any(), any()))
+        .thenReturn(Future.successful(validNpsIabd))
+
+      val result = controller().getEmployeeExpensesData(nino, taxYear, iabd)(fakeRequest)
+
+      status(result) mustBe OK
+
+      contentAsJson(result) mustBe validJson
+    }
+
+    "return BadRequest when bad request exception thrown" in {
+      val fakeRequest = FakeRequest("GET", "/", FakeHeaders(), any())
+
+      when(mockEmployeeExpensesService.getEmployeeExpenses(any(), any(), any()))
+        .thenReturn(Future.failed(new BadRequestException("")))
+
+      val result = controller().getEmployeeExpensesData(nino, taxYear, iabd)(fakeRequest)
+
+      status(result) mustBe BAD_REQUEST
+    }
+
+    "return NotFound when not found exception thrown" in {
+      val fakeRequest = FakeRequest("GET", "/", FakeHeaders(), any())
+
+      when(mockEmployeeExpensesService.getEmployeeExpenses(any(), any(), any()))
+        .thenReturn(Future.failed(new NotFoundException("")))
+
+      val result = controller().getEmployeeExpensesData(nino, taxYear, iabd)(fakeRequest)
+
+      status(result) mustBe NOT_FOUND
+    }
+
+    "return an Exception when an exception thrown" in {
+      val fakeRequest = FakeRequest("GET", "/", FakeHeaders(), any())
+
+      when(mockEmployeeExpensesService.getEmployeeExpenses(any(), any(), any()))
+        .thenReturn(Future.failed(new Exception("")))
+
+      val result = controller().getEmployeeExpensesData(nino, taxYear, iabd)(fakeRequest)
+
+      intercept[Exception] {
+        result mustBe an[Exception]
+      }
+    }
+  }
+}
