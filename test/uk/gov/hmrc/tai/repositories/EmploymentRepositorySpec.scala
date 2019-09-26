64f540ebdf32f15ad1efd4385ae9b56a33432571
John Lewis <23009934+lewey@users.noreply.github.com>
2019-09-26 15:21:53 +0100

    DDCNOS-5984: Fixed failing tests for EmploymentRepositorySpec

--- test/uk/gov/hmrc/tai/repositories/EmploymentRepositorySpec.scala
+++ test/uk/gov/hmrc/tai/repositories/EmploymentRepositorySpec.scala
@@ -32,6 +32,7 @@ import uk.gov.hmrc.tai.audit.Auditor
 import uk.gov.hmrc.tai.connectors.{CacheConnector, NpsConnector, RtiConnector}
 import uk.gov.hmrc.tai.model.TaiRoot
 import uk.gov.hmrc.tai.model.domain.{AnnualAccount, EndOfTaxYearUpdate, _}
+import uk.gov.hmrc.tai.model.error.EmploymentNotFound
 import uk.gov.hmrc.tai.model.tai.TaxYear
 
 import scala.concurrent.duration._
@@ -1524,9 +1525,9 @@ class EmploymentRepositorySpec extends PlaySpec with MockitoSugar {
       when(mockCacheConnector.findSeq[Employment](any(), any())(any())).thenReturn(Future.successful(List(emp1, emp2)))
 
       val sut = createSUT(mock[RtiConnector], mockCacheConnector, mock[NpsConnector], mock[Auditor])
-      Await.result(sut.employment(Nino(nino.nino), 4), 5 seconds) mustBe Some(emp1)
+      Await.result(sut.employment(Nino(nino.nino), 4), 5 seconds) mustBe Right(emp1)
     }
-    "return none when there is no employment found for that ID" in {
+    "return appropriate error type when there is no employment found for that ID" in {
       val emp1 = Employment(
         "TEST",
         Some("12345"),
@@ -1563,7 +1564,7 @@ class EmploymentRepositorySpec extends PlaySpec with MockitoSugar {
       when(mockCacheConnector.findSeq[Employment](any(), any())(any())).thenReturn(Future.successful(List(emp1, emp2)))
 
       val sut = createSUT(mock[RtiConnector], mockCacheConnector, mock[NpsConnector], mock[Auditor])
-      Await.result(sut.employment(Nino(nino.nino), 10), 5 seconds) mustBe None
+      Await.result(sut.employment(Nino(nino.nino), 10), 5 seconds) mustBe Left(EmploymentNotFound)
     }
     "get the current year employments from the hod" when {
       "data is in the cache for a year other than the current one and it does not contain the required employment" in {
