c3669042f27103fc747ea6cfe700291e5673a301
Steven Truong <45595406+oc-ste@users.noreply.github.com>
2019-12-17 14:56:58 +0000

    Auth recovers with Unauthorised result

--- app/uk/gov/hmrc/tai/controllers/predicates/AuthenticationPredicate.scala
+++ app/uk/gov/hmrc/tai/controllers/predicates/AuthenticationPredicate.scala
@@ -20,7 +20,7 @@ import javax.inject.{Inject, Singleton}
 import play.api.mvc._
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.auth.core.retrieve.~
-import uk.gov.hmrc.auth.core.{AuthorisedFunctions, ConfidenceLevel}
+import uk.gov.hmrc.auth.core.{AuthorisationException, AuthorisedFunctions, ConfidenceLevel}
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
@@ -34,19 +34,29 @@ class AuthenticationPredicate @Inject()(val authorisedFunctions: AuthorisedFunct
 
   def async(action: AuthenticatedRequest[AnyContent] => Future[Result]): Action[AnyContent] =
     Action.async { implicit request: Request[AnyContent] =>
-      authorisedFunctions.authorised(ConfidenceLevel.L100).retrieve(Retrievals.nino and Retrievals.trustedHelper) {
-        case _ ~ Some(trustedHelper) => action(AuthenticatedRequest(request, trustedHelper.principalNino))
-        case Some(nino) ~ _          => action(AuthenticatedRequest(request, Nino(nino)))
-        case _                       => throw new RuntimeException("Can't find valid credentials for user")
-      }
+      authorisedFunctions
+        .authorised(ConfidenceLevel.L100)
+        .retrieve(Retrievals.nino and Retrievals.trustedHelper) {
+          case _ ~ Some(trustedHelper) => action(AuthenticatedRequest(request, trustedHelper.principalNino))
+          case Some(nino) ~ _          => action(AuthenticatedRequest(request, Nino(nino)))
+          case _                       => throw new RuntimeException("Can't find valid credentials for user")
+        }
+        .recover {
+          case e: AuthorisationException => Unauthorized(e.getMessage)
+        }
     }
 
   def async[A](bodyParser: BodyParser[A])(action: AuthenticatedRequest[A] => Future[Result]): Action[A] =
     Action.async(bodyParser) { implicit request =>
-      authorisedFunctions.authorised(ConfidenceLevel.L100).retrieve(Retrievals.nino and Retrievals.trustedHelper) {
-        case _ ~ Some(trustedHelper) => action(AuthenticatedRequest(request, trustedHelper.principalNino))
-        case Some(nino) ~ _          => action(AuthenticatedRequest(request, Nino(nino)))
-        case _                       => throw new RuntimeException("Can't find valid credentials for user")
-      }
+      authorisedFunctions
+        .authorised(ConfidenceLevel.L100)
+        .retrieve(Retrievals.nino and Retrievals.trustedHelper) {
+          case _ ~ Some(trustedHelper) => action(AuthenticatedRequest(request, trustedHelper.principalNino))
+          case Some(nino) ~ _          => action(AuthenticatedRequest(request, Nino(nino)))
+          case _                       => throw new RuntimeException("Can't find valid credentials for user")
+        }
+        .recover {
+          case e: AuthorisationException => Unauthorized(e.getMessage)
+        }
     }
 }
