914364da5cc2eeea4b6d4bd75f2f090bb95102fe
Pascal Parois <pascal22p@parois.net>
2023-05-03 15:56:35 +0100

    more tests

---
+++ test/uk/gov/hmrc/tai/connectors/cache/CachingRtiConnectorSpec.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tai.connectors.cache
+
+import cats.data.EitherT
+import cats.implicits._
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, times, verify, when}
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.mongo.cache.DataKey
+import uk.gov.hmrc.tai.connectors.{CachingRtiConnector, ConnectorBaseSpec, RtiConnector}
+import uk.gov.hmrc.tai.model.domain.{AnnualAccount, Available, FourWeekly, Payment, RtiPaymentsForYearError, ServiceUnavailableError}
+import uk.gov.hmrc.tai.model.tai.TaxYear
+import uk.gov.hmrc.tai.repositories.cache.SessionCacheRepository
+
+import java.time.LocalDate
+import scala.concurrent.Future
+
+class CachingRtiConnectorSpec extends ConnectorBaseSpec {
+
+  override implicit lazy val app: Application = GuiceApplicationBuilder().overrides(
+    bind(classOf[RtiConnector])
+      .qualifiedWith("default")
+      .toInstance(mockRtiConnector),
+    bind[SessionCacheRepository].toInstance(mockSessionCacheRepository)
+  ).build()
+
+  val mockRtiConnector: RtiConnector = mock[RtiConnector]
+  val mockSessionCacheRepository: SessionCacheRepository  = mock[SessionCacheRepository]
+
+  override implicit val hc: HeaderCarrier         = HeaderCarrier()
+
+  override def beforeEach(): Unit = reset(mockRtiConnector, mockSessionCacheRepository)
+
+  def connector: CachingRtiConnector = app.injector.instanceOf[CachingRtiConnector]
+
+  implicit val userRequest: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
+
+  val payment = Payment(LocalDate.now(), 0, 0, 0, 0, 0, 0, FourWeekly, None)
+  val annualAccount = AnnualAccount(0, TaxYear(2020), Available, Seq(payment), Seq.empty)
+
+
+  "Calling CachingRtiConnector.getPaymentsForYear" must {
+    "return a Right Seq[AnnualAccount] object" when {
+      "no value is cached" in {
+        val expected = Seq(annualAccount)
+
+        when(mockSessionCacheRepository.getFromSession[Seq[AnnualAccount]](DataKey(any[String]()))(any(), any()))
+          .thenReturn(Future.successful(None))
+
+        when(
+          mockSessionCacheRepository.putSession[Seq[AnnualAccount]](DataKey(any[String]()), any())(any(), any(), any())
+        )
+          .thenReturn(Future.successful(("", "")))
+
+        when(mockRtiConnector.getPaymentsForYear(any(), any())(any(), any()))
+          .thenReturn(EitherT.rightT[Future, RtiPaymentsForYearError](expected))
+
+        val result = connector.getPaymentsForYear(nino, TaxYear()).value.futureValue
+
+        result mustBe Right(expected)
+
+        verify(mockSessionCacheRepository, times(1))
+          .getFromSession[Seq[AnnualAccount]](DataKey(any[String]()))(any(), any())
+
+        verify(mockSessionCacheRepository, times(1))
+          .putSession[Seq[AnnualAccount]](DataKey(any[String]()), any())(any(), any(), any())
+
+        verify(mockRtiConnector, times(1)).getPaymentsForYear(any(), any())(any(), any())
+      }
+
+      "a value is cached" in {
+        val expected = Seq(annualAccount)
+
+        when(mockSessionCacheRepository.getFromSession[Seq[AnnualAccount]](DataKey(any[String]()))(any(), any()))
+          .thenReturn(Future.successful(Some(expected)))
+
+        when(mockRtiConnector.getPaymentsForYear(any(), any())(any(), any()))
+          .thenReturn(null)
+
+        when(
+          mockSessionCacheRepository.putSession[Seq[AnnualAccount]](DataKey(any[String]()), any())(any(), any(), any())
+        )
+          .thenReturn(null)
+
+        val result = connector.getPaymentsForYear(nino, TaxYear()).value.futureValue
+
+        result mustBe Right(expected)
+
+        verify(mockSessionCacheRepository, times(1))
+          .getFromSession[Seq[AnnualAccount]](DataKey(any[String]()))(any(), any())
+
+        verify(mockSessionCacheRepository, times(0))
+          .putSession[Seq[AnnualAccount]](DataKey(any[String]()), any())(any(), any(), any())
+
+        verify(mockRtiConnector, times(0)).getPaymentsForYear(any(), any())(any(), any())
+      }
+    }
+
+    "return a Left RtiPaymentsForYearError object" in {
+      when(mockRtiConnector.getPaymentsForYear(any(), any())(any(), any()))
+        .thenReturn(EitherT.leftT[Future, Seq[AnnualAccount]](ServiceUnavailableError: RtiPaymentsForYearError))
+
+      when(mockSessionCacheRepository.getFromSession[Seq[AnnualAccount]](DataKey(any[String]()))(any(), any()))
+        .thenReturn(Future.successful(None))
+
+      when(
+        mockSessionCacheRepository.putSession[Seq[AnnualAccount]](DataKey(any[String]()), any())(any(), any(), any())
+      )
+        .thenReturn(Future.successful(("", "")))
+
+
+      val result = connector.getPaymentsForYear(nino, TaxYear()).value.futureValue
+      result mustBe a[Left[_, _]]
+    }
+  }
+}
