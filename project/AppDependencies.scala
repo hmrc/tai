64ca0819452c33c1f56f7518512f50f30d6dcb3b
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2024-03-22 10:58:20 +0000

    DDCNL-8632: added crypto dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,17 +12,18 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-backend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc"       %% s"domain-$playVersion"                       % "9.0.0",
+    "uk.gov.hmrc"       %% "crypto"                                     % "7.6.0",
     "org.typelevel"     %% "cats-core"                                  % "2.10.0",
     "uk.gov.hmrc"       %% s"mongo-feature-toggles-client-$playVersion" % "1.3.0",
     "org.typelevel"     %% "cats-effect"                                % "3.5.4"
   )
 
-  val compileTest: Seq[ModuleID] = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "org.mockito"            %% "mockito-scala-scalatest"       % "1.17.30",
     "org.scalatestplus"      %% "scalacheck-1-17"               % "3.2.18.0",
     "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
   ).map(_  % "test")
 
-  val all: Seq[ModuleID] = compile ++ compileTest
+  val all: Seq[ModuleID] = compile ++ test
 }
