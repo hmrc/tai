e7d272eead9f9378a27f37e5e58a96c13867ff6b
grantrobson <grant.robson@digital.hmrc.gov.uk>
2024-06-20 08:33:33 +0100

    8108 App conf defaults

--- app/uk/gov/hmrc/tai/config/ApplicationConfig.scala
+++ app/uk/gov/hmrc/tai/config/ApplicationConfig.scala
@@ -88,7 +88,7 @@ class MongoConfig @Inject() (val runModeConfiguration: Configuration) extends Ba
   lazy val mongoEncryptionEnabled: Boolean =
     runModeConfiguration.getOptional[Boolean]("mongo.encryption.enabled").getOrElse(true)
   lazy val mongoTTL: Int = runModeConfiguration.getOptional[Int]("tai.cache.expiryInSeconds").getOrElse(900)
-  lazy val mongoLockTTL: Int = runModeConfiguration.getOptional[Int]("mongo.lock.expiryInMicroseconds").getOrElse(2000)
+  lazy val mongoLockTTL: Int = runModeConfiguration.getOptional[Int]("mongo.lock.expiryInMicroseconds").getOrElse(1200)
   lazy val mongoTTLUpdateIncome: Int =
     runModeConfiguration.getOptional[Int]("tai.cache.updateIncome.expiryInSeconds").getOrElse(3600 * 48)
 }
