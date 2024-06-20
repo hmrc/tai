e74227e9d8929614613bab6495d021275e49fad0
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-06-20 10:57:56 +0100

    DDCNL-9006: move auth to pertax/authorise (#294)
    * DDCNL-9006: move auth to pertax/authorise

--- test/uk/gov/hmrc/tai/util/BaseSpec.scala
+++ test/uk/gov/hmrc/tai/util/BaseSpec.scala
@@ -17,6 +17,7 @@
 package uk.gov.hmrc.tai.util
 
 import org.mockito.MockitoSugar
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.PlaySpec
 import play.api.Application
@@ -24,19 +25,34 @@ import play.api.cache.AsyncCacheApi
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Injecting
 import play.api.inject.bind
+import play.api.mvc.ControllerComponents
+import play.api.test.Helpers.stubControllerComponents
+import uk.gov.hmrc.domain.{Generator, Nino}
+import uk.gov.hmrc.http.{HeaderCarrier, SessionId}
+import uk.gov.hmrc.tai.connectors.cache.CacheId
 import uk.gov.hmrc.tai.controllers.FakeTaiPlayApplication
-import uk.gov.hmrc.tai.mocks.MockAuthenticationPredicate
+import uk.gov.hmrc.tai.controllers.auth.AuthJourney
 
 import scala.concurrent.ExecutionContext
+import scala.util.Random
 
 trait BaseSpec
-    extends PlaySpec with MockitoSugar with MockAuthenticationPredicate with FakeTaiPlayApplication with ScalaFutures
-    with Injecting {
+    extends PlaySpec with MockitoSugar with FakeTaiPlayApplication with ScalaFutures with Injecting
+    with BeforeAndAfterEach {
 
   implicit lazy val ec: ExecutionContext = inject[ExecutionContext]
   val responseBody: String = ""
 
   lazy val fakeAsyncCacheApi = new FakeAsyncCacheApi()
+
+  lazy val loggedInAuthenticationAuthJourney: AuthJourney = FakeAuthJourney
+  lazy val cc: ControllerComponents = stubControllerComponents()
+  val sessionIdValue: String = "some session id"
+  implicit lazy val hc: HeaderCarrier = HeaderCarrier(sessionId = Some(SessionId(sessionIdValue)))
+  val nino: Nino = new Generator(Random).nextNino
+  val otherNino: Nino = new Generator(Random).nextNino
+  val cacheId: CacheId = CacheId(nino)
+  val cacheIdNoSession: CacheId = CacheId.noSession(nino)
 
   override implicit lazy val app: Application =
     GuiceApplicationBuilder()
