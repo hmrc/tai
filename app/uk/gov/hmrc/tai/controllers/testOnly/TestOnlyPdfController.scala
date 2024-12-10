7c52794a93e1e0301759eaa0ea06a67656c3bda4
Aivars Smaukstelis <aivars.smaukstelis@digital.hmrc.gov.uk>
2024-12-10 15:38:23 +0000

    example form - old pdf writer

--- app/uk/gov/hmrc/tai/controllers/testOnly/TestOnlyPdfController.scala
+++ app/uk/gov/hmrc/tai/controllers/testOnly/TestOnlyPdfController.scala
@@ -21,9 +21,9 @@ import org.apache.pekko.util.ByteString
 import play.api.http.HttpEntity.Strict
 import play.api.mvc._
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import uk.gov.hmrc.tai.model.templates.EmploymentPensionViewModel
+import uk.gov.hmrc.tai.model.templates.{EmploymentPensionViewModel, RemoveCompanyBenefitViewModel}
 import uk.gov.hmrc.tai.service.PdfService
-import uk.gov.hmrc.tai.templates.html.{EmploymentIForm, PensionProviderIForm}
+import uk.gov.hmrc.tai.templates.html.{EmploymentIForm, PensionProviderIForm, RemoveCompanyBenefitIForm}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -46,22 +46,33 @@ class TestOnlyPdfController @Inject() (
 
   def generatePdf(isNewGenerator: Boolean, contentId: String): Future[Array[Byte]] = {
     val content = contentId match {
-      case "PensionProviderIForm" => Future.successful(exampleContent_PensionProviderIForm)
-      case _                      => Future.successful("<html><body>test</body></html>")
+      case "EmploymentIForm"           => Future.successful(exampleContent_EmploymentIForm)
+      case "PensionProviderIForm"      => Future.successful(exampleContent_PensionProviderIForm)
+      case "RemoveCompanyBenefitIForm" => Future.successful(exampleContent_PensionProviderIForm)
+      case _                           => Future.successful("<html><body>test</body></html>")
     }
     content.flatMap(ps.generatePdf)
   }
 
+  // http://localhost:9331/tai/test-only/pdf/false/EmploymentIForm
+  // http://localhost:9331/tai/test-only/pdf/true/EmploymentIForm
+  def exampleContent_EmploymentIForm = EmploymentIForm(exampleData_EmploymentPensionViewModel).toString()
+
   // http://localhost:9331/tai/test-only/pdf/false/PensionProviderIForm
   // http://localhost:9331/tai/test-only/pdf/true/PensionProviderIForm
   def exampleContent_PensionProviderIForm = PensionProviderIForm(exampleData_EmploymentPensionViewModel).toString()
+
+  // http://localhost:9331/tai/test-only/pdf/false/RemoveCompanyBenefitIForm
+  // http://localhost:9331/tai/test-only/pdf/true/RemoveCompanyBenefitIForm
+  def exampleContent_RemoveCompanyBenefitIForm =
+    RemoveCompanyBenefitIForm(exampleData_RemoveCompanyBenefitViewModel).toString()
 
   def exampleData_EmploymentPensionViewModel = EmploymentPensionViewModel(
     "6 April 2017 to 5 April 2018",
     "AA000000A",
     "firstname",
     "lastname",
-    "1 Jan 1900",
+    "1 January 1900",
     "Yes",
     "0123456789",
     "address line 1",
@@ -73,9 +84,28 @@ class TestOnlyPdfController @Inject() (
     "No",
     "pension name",
     "99999999999",
-    "2 Feb 2000",
+    "2 February 2000",
     "3 March 2100",
     "my story"
+  )
+
+  def exampleData_RemoveCompanyBenefitViewModel = RemoveCompanyBenefitViewModel(
+    "AA000000A",
+    "firstname",
+    "lastname",
+    "1 January 1900",
+    "Yes",
+    "0123456789",
+    "addressLine1",
+    "addressLine2",
+    "addressLine3",
+    "postcode",
+    "No",
+    "Yes",
+    "No",
+    "Mileage",
+    "1000000",
+    "On or after 3 March 2100"
   )
 
 }
