7b643d9540f5466521c15f209efe9ce0341ec955
Aivars Smaukstelis <aivars.smaukstelis@digital.hmrc.gov.uk>
2024-12-19 14:58:02 +0000

    adding test only index page

--- app/uk/gov/hmrc/tai/controllers/testOnly/TestOnlyPdfController.scala
+++ app/uk/gov/hmrc/tai/controllers/testOnly/TestOnlyPdfController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,10 @@ package uk.gov.hmrc.tai.controllers.testOnly
 import com.google.inject.{Inject, Singleton}
 import org.apache.pekko.util.ByteString
 import play.api.http.HttpEntity.Strict
-import play.api.mvc._
+import play.api.mvc.{Action, _}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import uk.gov.hmrc.tai.model.templates.{EmploymentPensionViewModel, RemoveCompanyBenefitViewModel}
 import uk.gov.hmrc.tai.service.PdfService
-import uk.gov.hmrc.tai.templates.html.{EmploymentIForm, PensionProviderIForm, RemoveCompanyBenefitIForm}
-import uk.gov.hmrc.tai.templates.xml.{EmploymentXmlFoForm, HelloForm, PensionProviderXmlFoForm, RemoveCompanyBenefitXmlFoForm}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -36,40 +34,83 @@ class TestOnlyPdfController @Inject() (
 )(implicit ec: ExecutionContext)
     extends BackendController(cc) {
 
-  def pdf( contentId: String = "new:hello:default"): Action[AnyContent] = Action.async { _ =>
-    println(s"--------------TestOnlyPdfController.pdf($contentId)")
-
-
-
-
-  }
-
-  def generatePdf(isNewGenerator: Boolean, contentId: String): Future[Array[Byte]] = {
-    val content = contentId match {
-      // new
-      case "hello"                         => Future.successful(example_hello)
-      case "EmploymentXmlFoForm"           => Future.successful(exampleContent_EmploymentXmlFoForm)
-      case "PensionProviderXmlFoForm"      => Future.successful(exampleContent_PensionProviderXmlFoForm)
-      case "RemoveCompanyBenefitXmlFoForm" => Future.successful(exampleContent_RemoveCompanyBenefitXmlFoForm)
-      case "RemoveCompanyBenefitXmlFoFormWithDetails" =>
-        Future.successful(exampleContent_RemoveCompanyBenefitXmlFoFormWithDetails)
-      // old
-      case "EmploymentIForm"           => Future.successful(exampleContent_EmploymentIForm)
-      case "PensionProviderIForm"      => Future.successful(exampleContent_PensionProviderIForm)
-      case "RemoveCompanyBenefitIForm" => Future.successful(exampleContent_RemoveCompanyBenefitIForm)
-      case "RemoveCompanyBenefitIFormWithDetails" =>
-        Future.successful(exampleContent_RemoveCompanyBenefitIFormWithDetails)
-      case "hello" => Future.successful("<html><body>test</body></html>")
+  def index(): Action[AnyContent] =
+    Action { _ =>
+      Result(
+        header = ResponseHeader(OK),
+        body = Strict(
+          ByteString.apply(uk.gov.hmrc.tai.templates.html.Index().body.getBytes()),
+          contentType = Some("text/html")
+        )
+      )
     }
-    content.flatMap(html =>
+
+  def pdf(isNewGenerator: Boolean = false, formId: String = "default"): Action[AnyContent] =
+    Action.async { _ =>
+      println(s"--------------TestOnlyPdfController.pdf(isNewGenerator:$isNewGenerator,formId:$formId)")
+      generatePdf(isNewGenerator, formId).map(bytes =>
+        Result(
+          header = ResponseHeader(OK),
+          body = Strict(ByteString.apply(bytes), contentType = Some("application/pdf"))
+        )
+      )
+    }
+
+  def generatePdf(isNewGenerator: Boolean, formId: String): Future[Array[Byte]] = {
+
+    val generator: String => Future[Array[Byte]] = (content: String) =>
       if (isNewGenerator)
-        Future.successful(contemporaryPdfService.toPdfBytes(html))
+        Future.successful(contemporaryPdfService.toPdfBytes(content))
       else
-        legacyPdfService.generatePdf(html)
-    )
+        legacyPdfService.generatePdf(content)
+
+    def fillDefault(): String =
+      if (isNewGenerator)
+        uk.gov.hmrc.tai.templates.xml.HelloForm().body
+      else
+        uk.gov.hmrc.tai.templates.html.HelloForm().body
+
+    def fillEmploymentPensionTemplate(model: EmploymentPensionViewModel): String =
+      if (isNewGenerator)
+        uk.gov.hmrc.tai.templates.xml.EmploymentIForm(model).body
+      else
+        uk.gov.hmrc.tai.templates.html.EmploymentIForm(model).body
+
+    def fillRemoveCompanyBenefitTemplate(model: RemoveCompanyBenefitViewModel): String =
+      if (isNewGenerator)
+        uk.gov.hmrc.tai.templates.xml.RemoveCompanyBenefitIForm(model).body
+      else
+        uk.gov.hmrc.tai.templates.html.RemoveCompanyBenefitIForm(model).body
+
+    def fillPensionProviderTemplate(model: EmploymentPensionViewModel): String =
+      if (isNewGenerator)
+        uk.gov.hmrc.tai.templates.xml.PensionProviderIForm(model).body
+      else
+        uk.gov.hmrc.tai.templates.html.PensionProviderIForm(model).body
+
+    var formSource: String =
+      formId match {
+        case "Employment_emp_isEnd_NO_isAdd_NO" =>
+          fillEmploymentPensionTemplate(emp_isEnd_NO_isAdd_NO)
+        case "Employment_emp_isEnd_YES_isAdd_NO" =>
+          fillEmploymentPensionTemplate(emp_isEnd_YES_isAdd_NO)
+        case "Employment_emp_isEnd_NO_isAdd_YES" =>
+          fillEmploymentPensionTemplate(emp_isEnd_NO_isAdd_YES)
+        case "PensionProvider_emp_isEnd_NO_isAdd_NO" =>
+          fillPensionProviderTemplate(emp_isEnd_NO_isAdd_NO)
+        case "RemoveCompanyBenefit_rcb_isEnd_NO" =>
+          fillRemoveCompanyBenefitTemplate(rcb_isEnd_NO)
+        case "RemoveCompanyBenefit_rcb_isEnd_YES" =>
+          fillRemoveCompanyBenefitTemplate(rcb_isEnd_YES)
+        case _ =>
+          fillDefault()
+      }
+
+    generator.apply(formSource)
+
   }
 
-  def exampleData_EmploymentPensionViewModel = EmploymentPensionViewModel(
+  var emp_isEnd_NO_isAdd_NO = EmploymentPensionViewModel(
     "6 April 2017 to 5 April 2018",
     "AA000000A",
     "firstname",
@@ -82,7 +123,7 @@ class TestOnlyPdfController @Inject() (
     "address line 3",
     "postcode",
     "No",
-    "Yes",
+    "No",
     "No",
     "pension name",
     "99999999999",
@@ -91,7 +132,13 @@ class TestOnlyPdfController @Inject() (
     "my story"
   )
 
-  def exampleData_RemoveCompanyBenefitViewModel = RemoveCompanyBenefitViewModel(
+  var emp_isEnd_YES_isAdd_NO =
+    emp_isEnd_NO_isAdd_NO.copy(isEnd = "Yes")
+
+  var emp_isEnd_NO_isAdd_YES =
+    emp_isEnd_NO_isAdd_NO.copy(isAdd = "Yes")
+
+  val rcb_isEnd_NO = RemoveCompanyBenefitViewModel(
     "AA000000A",
     "firstname",
     "lastname",
@@ -110,23 +157,6 @@ class TestOnlyPdfController @Inject() (
     "On or after 3 March 2100"
   )
 
-  def exampleData_RemoveCompanyBenefitViewModel_details = RemoveCompanyBenefitViewModel(
-    "AA000000A",
-    "firstname",
-    "lastname",
-    "1 January 1900",
-    "Yes",
-    "0123456789",
-    "addressLine1",
-    "addressLine2",
-    "addressLine3",
-    "postcode",
-    "No",
-    "Yes",
-    "Yes",
-    "Mileage",
-    "1000000",
-    "On or after 3 March 2100"
-  )
+  val rcb_isEnd_YES = rcb_isEnd_NO.copy(isEnd = "Yes")
 
 }
