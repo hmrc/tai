cbd812fab5c859b5ae31deb96c256d39509a4fb6
grantrobson <grant.robson@digital.hmrc.gov.uk>
2025-03-17 14:47:13 +0000

    10202 Uncomment code

--- test/uk/gov/hmrc/tai/model/domain/IabdDetailsSpec.scala
+++ test/uk/gov/hmrc/tai/model/domain/IabdDetailsSpec.scala
@@ -47,58 +47,58 @@ class IabdDetailsSpec extends PlaySpec {
     Some(LocalDate.of(2024, 2, 10))
   )
 
-//  "IabdDetailsToggleOff" must {
-//
-//    "deserialize JSON correctly" in {
-//      sampleJsonToggleOff.validate[Seq[IabdDetails]](IabdDetailsToggleOff.reads) mustBe JsSuccess(
-//        Seq(expectedModelToggleOff)
-//      )
-//    }
-//
-//    "serialize model to JSON correctly" in {
-//      val expectedJson = Json.parse(
-//        """
-//          |{
-//          |  "nino": "AB123456C",
-//          |  "employmentSequenceNumber": 12345,
-//          |  "source": 1,
-//          |  "type": 100,
-//          |  "receiptDate": "01/02/2024",
-//          |  "captureDate": "10/02/2024"
-//          |}
-//          |""".stripMargin
-//      )
-//
-//      Json.toJson(expectedModelToggleOff)(IabdDetailsToggleOff.format) mustBe expectedJson
-//    }
-//  }
+  "IabdDetailsToggleOff" must {
+
+    "deserialize JSON correctly" in {
+      sampleJsonToggleOff.validate[Seq[IabdDetails]](IabdDetailsToggleOff.reads) mustBe JsSuccess(
+        Seq(expectedModelToggleOff)
+      )
+    }
+
+    "serialize model to JSON correctly" in {
+      val expectedJson = Json.parse(
+        """
+          |{
+          |  "nino": "AB123456C",
+          |  "employmentSequenceNumber": 12345,
+          |  "source": 1,
+          |  "type": 100,
+          |  "receiptDate": "01/02/2024",
+          |  "captureDate": "10/02/2024"
+          |}
+          |""".stripMargin
+      )
+
+      Json.toJson(expectedModelToggleOff)(IabdDetailsToggleOff.format) mustBe expectedJson
+    }
+  }
 
   "IabdDetailsToggleOn" must {
 
-//    "deserialize JSON correctly when unknown source type" in {
-//      val sampleJsonToggleOn = Json.obj(
-//        "iabdDetails" -> Json.arr(
-//          Json.obj(
-//            "nationalInsuranceNumber"  -> "AB123456C",
-//            "employmentSequenceNumber" -> 12345,
-//            "source"                   -> "unknown source type",
-//            "type"                     -> "Non-Coded Income (019)"
-//          )
-//        )
-//      )
-//      val expectedModelToggleOn: IabdDetails = IabdDetails(
-//        nino = Some("AB123456C"),
-//        employmentSequenceNumber = Some(12345),
-//        source = None,
-//        `type` = Some(19),
-//        receiptDate = None,
-//        captureDate = None
-//      )
-//      sampleJsonToggleOn.validate[Seq[IabdDetails]](IabdDetailsToggleOn.reads) mustBe JsSuccess(
-//        Seq(expectedModelToggleOn),
-//        JsPath \ "iabdDetails"
-//      )
-//    }
+    "deserialize JSON correctly when unknown source type" in {
+      val sampleJsonToggleOn = Json.obj(
+        "iabdDetails" -> Json.arr(
+          Json.obj(
+            "nationalInsuranceNumber"  -> "AB123456C",
+            "employmentSequenceNumber" -> 12345,
+            "source"                   -> "unknown source type",
+            "type"                     -> "Non-Coded Income (019)"
+          )
+        )
+      )
+      val expectedModelToggleOn: IabdDetails = IabdDetails(
+        nino = Some("AB123456C"),
+        employmentSequenceNumber = Some(12345),
+        source = None,
+        `type` = Some(19),
+        receiptDate = None,
+        captureDate = None
+      )
+      sampleJsonToggleOn.validate[Seq[IabdDetails]](IabdDetailsToggleOn.reads) mustBe JsSuccess(
+        Seq(expectedModelToggleOn),
+        JsPath \ "iabdDetails"
+      )
+    }
 
     "deserialize JSON correctly when source type is missing" in {
       val sampleJsonToggleOn = Json.obj(
