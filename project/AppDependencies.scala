83d10cfb1c9575dfcc60a74fc02ccc6a8b1d8570
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-08-04 14:13:28 +0100

    updated bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "9.17.0"
+  private val bootstrapVersion = "9.18.0"
 
   val compile: Seq[ModuleID] = Seq(
     filters,
